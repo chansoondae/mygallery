25490414e809fe0a2068edc3283238ac8fcdbfb4
Chansu Suh <chansusuh@Chansuui-noteubug.local>
2024-06-28 21:24:16 +0900

    second commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,15 @@
-// import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-//     })
-//   ]
-// })
+const { GOOGLE_CLIENT_ID = '', GOOGLE_CLIENT_SECRET = '' } = process.env;
 
-// export { handler as GET, handler as POST }
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET
+    })
+  ]
+})
+
+export { handler as GET, handler as POST }
